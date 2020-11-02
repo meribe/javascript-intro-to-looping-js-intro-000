d66698f8bdbaee38f9c7f667de99541c460466d5
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-11-02 17:22:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 let array = [];
 function forLoop(array) {
   for (let i=0; i<25; i++){
-    if (i===1 && i===0){
+    if (i===1){
       array.push( "I am 1 strange loop.")
   }  else; {
       array.push(`I am ${i} strange loops.`)
